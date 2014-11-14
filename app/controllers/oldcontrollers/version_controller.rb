8f361772125ef21dfa05ea63a396577021d4b13d
ouruboros <r@aurora.(none)>
2014-11-14 03:35:08 +0100

    uy

--- app/controllers/oldcontrollers/version_controller.rb
+++
@@ -1,8 +0,0 @@
-class VersionController < ApplicationController
-  before_filter :require_user, :only => [:number]
-
-  def number
-    render :json => {:version_number => '0.2'}, :status => :ok
-  end
-
-end
